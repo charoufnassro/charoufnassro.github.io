9b03e13eb3688097d7a25d5109efb7e8f4d7943d
charoufnassro <charoufnassro@gmail.com>
2019-06-29 16:08:10 +0200

    robofriend project react

---
+++ robofriends/precache-manifest.a07445faa1733c27f1e68ac00e3fcbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991adf95e80270f0954a914a8a0de833",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a8eff65d35dd360ce854",
+    "url": "/robofriends/static/css/main.4e450a56.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "a8eff65d35dd360ce854",
+    "url": "/robofriends/static/js/main.56d671f5.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  }
+]);
