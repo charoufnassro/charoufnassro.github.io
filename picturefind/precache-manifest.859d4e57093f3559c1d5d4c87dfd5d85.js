d28afb2b8dd68b834cb18070ebd2290f9c69fc7f
charoufnassro <charoufnassro@gmail.com>
2019-06-29 15:30:46 +0200

    Add Picture Find React Project

---
+++ picturefind/precache-manifest.859d4e57093f3559c1d5d4c87dfd5d85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1674a4d12f54688ff641d70b4eaa410a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1b4ec8c78e994af80c4",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3859223799148045be97",
+    "url": "/static/css/main.2e48cbca.chunk.css"
+  },
+  {
+    "revision": "f1b4ec8c78e994af80c4",
+    "url": "/static/js/2.30ffa0c1.chunk.js"
+  },
+  {
+    "revision": "3859223799148045be97",
+    "url": "/static/js/main.f8d12a40.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a31de4509689954a4d7080a88cb4c18a",
+    "url": "/static/media/share.a31de450.svg"
+  }
+]);
